1bf4a623be4505c16ba1fea320f2f8ab31a3bb7c
Chris Mitchell <chris@ginkgobioworks.com>
2016-03-02 14:59:46 -0500

    wget filepath

--- ci/travis_downloaddata.sh
+++ ci/travis_downloaddata.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 BASEDIR=$PWD
 DATADIR="$BASEDIR/../pyquant/tests/data"
-if [ ! -e "$DATADIR/Chris_Ecoli_1-2-4-(01).msf" ]; then
-	wget "ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/03/PXD003327/Chris_Ecoli_1-2-4-(01).msf"
+MSFFILE="$DATADIR/Chris_Ecoli_1-2-4-(01).msf"
+MZMLFILE="$DATADIR/Chris_Ecoli_1-2-4.mzML"
+if [ ! -e $MSFFILE ]; then
+	wget -O $MSFFILE "ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/03/PXD003327/Chris_Ecoli_1-2-4-(01).msf"
 fi
-if [ ! -e "$DATADIR/Chris_Ecoli_1-2-4.mzML" ]; then
-        wget "ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/03/PXD003327/Chris_Ecoli_1-2-4.mzML"
+if [ ! -e $MZMLFILE ]; then
+        wget -O $MZMLFILE "ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/03/PXD003327/Chris_Ecoli_1-2-4.mzML"
 fi
