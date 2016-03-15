7956edcd329dfaf9cf0fdab8110eba10fe97c51b
Chris Mitchell <chris@ginkgobioworks.com>
2016-03-15 12:24:28 -0400

    version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='pyquant-ms',
-    version='0.1.41',
+    version='0.1.42',
     packages=find_packages(),
     scripts=['scripts/pyQuant'],
     entry_points={'console_scripts': ['pyQuant = pyquant.command_line:run_pyquant',]},
