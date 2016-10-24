f0fe64057ab248c29ac37c2197a0d18ac3b9c91b
Chris <chris.mit7@gmail.com>
2016-10-24 13:16:01 -0400

    Fix linux install

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import os
 from distutils.core import setup
+from distutils.extension import Extension
 from setuptools import find_packages
 try:
     from Cython.Build import cythonize
@@ -26,7 +27,7 @@ setup(
     packages=find_packages(),
     scripts=['scripts/pyQuant'],
     entry_points={'console_scripts': ['pyQuant = pyquant.command_line:run_pyquant',]},
-    install_requires = ['cython', 'numpy', 'scipy >= 0.18.*', 'patsy', 'pythomics >= 0.3.41', 'pandas', 'lxml', 'scikit-learn', 'simplejson'],
+    install_requires=['cython', 'numpy', 'scipy >= 0.18.*', 'patsy', 'pythomics >= 0.3.41', 'pandas', 'lxml', 'scikit-learn', 'simplejson'],
     include_package_data=True,
     description='A framework for the analysis of quantitative mass spectrometry data',
     url='http://www.github.com/pandeylab/pyquant',
@@ -36,7 +37,11 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
+    setup_requires=[
+        'cython',
+    ],
     cmdclass={'build_ext': Cython.Distutils.build_ext} if CYTHON else {},
-    ext_modules = cythonize("pyquant/*.pyx") if CYTHON else [],
-    include_dirs = [numpy.get_include()] if NUMPY else [],
+    extensions = [Extension("*", ["*.pyx"])],
+    # ext_modules=cythonize("pyquant/*.pyx") if CYTHON else [],
+    include_dirs=[numpy.get_include()] if NUMPY else [],
 )
