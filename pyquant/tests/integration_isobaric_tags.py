928b9c7953bf3f04d18d2235a82a5c2114ca3935
Chris <chris.mit7@gmail.com>
2017-04-12 09:11:51 -0400

    Error tolerant test for itraq

--- pyquant/tests/integration_isobaric_tags.py
+++ pyquant/tests/integration_isobaric_tags.py
@@ -16,10 +16,26 @@ class ITraqTest(mixins.FileMixins, unittest.TestCase):
         com = [self.executable, '--scan-file', self.itraq_mzml, '-o', self.output, '--precursor-ppm', '200', '--isobaric-tags', '--label-method', 'iTRAQ4']
         subprocess.call(com)
         data = pd.read_table(self.output)
-        self.assertListEqual([int(i) for i in data['114 Intensity'].values.tolist()], [1215, 9201, 1218, 83983, 10266, 2995, 7160])
-        self.assertListEqual([int(i) for i in data['115 Intensity'].values.tolist()], [1428, 38772, 946, 1161, 12032, 4109, 8421])
-        self.assertListEqual([int(i) for i in data['116 Intensity'].values.tolist()], [1031, 15314, 0, 94243, 11381, 3350, 8577])
-        self.assertListEqual([int(i) for i in data['117 Intensity'].values.tolist()], [0, 42004, 1032, 383, 14290, 2557, 8969])
+        for i,j in zip([int(i) for i in data['114 Intensity'].values.tolist()], [1215, 9201, 1218, 83983, 10266, 2995, 7160]):
+            delta = j*0.1
+            if delta != 0 and delta < 300:
+                delta = 300
+            self.assertAlmostEqual(i, j, delta=delta)
+        for i, j in zip([int(i) for i in data['115 Intensity'].values.tolist()], [1428, 38772, 946, 1161, 12032, 4109, 8421]):
+            delta = j * 0.1
+            if delta != 0 and delta < 300:
+                delta = 300
+            self.assertAlmostEqual(i, j, delta=delta)
+        for i, j in zip([int(i) for i in data['116 Intensity'].values.tolist()], [1031, 15314, 0, 94243, 11381, 3350, 8577]):
+            delta = j * 0.1
+            if delta != 0 and delta < 300:
+                delta = 300
+            self.assertAlmostEqual(i, j, delta=delta)
+        for i, j in zip([int(i) for i in data['117 Intensity'].values.tolist()], [0, 42004, 1032, 383, 14290, 2557, 8969]):
+            delta = j * 0.1
+            if delta != 0 and delta < 300:
+                delta = 300
+            self.assertAlmostEqual(i, j, delta=delta)
 
 if __name__ == '__main__':
     unittest.main()
