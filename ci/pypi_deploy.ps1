3aa0a1cd2fae0fde576bb562971fa5e489a35199
Chris Mitchell <chris.mit7@gmail.com>
2020-01-04 18:38:48 -0500

    Remove unused files

--- ci/pypi_deploy.ps1
+++
@@ -1,4 +0,0 @@
-if ($env:appveyor_repo_tag -eq "True") {
-  (Get-Content ci\.pypirc) | Foreach-Object {$_ -replace '%PASS%',$env:PYPI_PASS} | Set-Content $env:userprofile\.pypirc
-  Invoke-Expression "$env:CMD_IN_ENV python setup.py bdist_wheel upload"
-}
