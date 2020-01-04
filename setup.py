d1948d5b4ba06e6ce7ee3372140b05375aba30de
Chris Mitchell <chris.mit7@gmail.com>
2020-01-03 19:15:31 -0500

    Bump version: 0.3.0 → 0.3.0-beta

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="pyquant",
-    version="0.3.0",
+    version="0.3.0-beta",
     packages=find_packages(),
     scripts=["scripts/pyQuant"],
     entry_points={"console_scripts": ["pyQuant = pyquant.command_line:run_pyquant",]},
